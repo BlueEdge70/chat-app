e88b134e2c93aea8ab6082bb9f895cc7b53fe4d1
Rajpanchalll <panchalraj512@gmail.com>
2025-01-04 18:55:36 +0530

    Setup folder structure for quick chat app

---
+++ angular/src/app/Components/chat/personal-chat/personal-chat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-chat',
+  standalone: true,
+  imports: [],
+  templateUrl: './personal-chat.component.html',
+  styleUrl: './personal-chat.component.scss'
+})
+export class PersonalChatComponent {
+
+}
