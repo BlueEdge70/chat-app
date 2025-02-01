55ece61042e51f8336dad77ff2301593c2d8be18
Rajpanchalll <panchalraj512@gmail.com>
2025-02-01 19:35:50 +0530

    Disabled the websockets

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -13,30 +13,32 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
 
-  private websocketService: WebSocketService = inject(WebSocketService);
+  // private websocketService: WebSocketService = inject(WebSocketService);
 
-  ngOnInit(): void {
-    this.initializeSocketConnection();
-  }
+  // Comment the web socket code for now. will move to this module once we finish other todo's
 
-  ngOnDestroy() {
-    this.disconnectSocket();
-  }
+  // ngOnInit(): void {
+  //   this.initializeSocketConnection();
+  // }
 
-  // Initializes socket connection
-  initializeSocketConnection() {
-    this.websocketService.connectSocket('message');
-  }
+  // ngOnDestroy() {
+  //   this.disconnectSocket();
+  // }
 
-  // Receives response from socket connection 
-  receiveSocketResponse() {
-    this.websocketService.receiveStatus().subscribe((receivedMessage: string) => {
-      console.log(receivedMessage);
-    });
-  }
+  // // Initializes socket connection
+  // initializeSocketConnection() {
+  //   this.websocketService.connectSocket('message');
+  // }
 
-  // Disconnects socket connection
-  disconnectSocket(): any {
-    return this.websocketService.disconnetSocket();
-  }
+  // // Receives response from socket connection 
+  // receiveSocketResponse() {
+  //   this.websocketService.receiveStatus().subscribe((receivedMessage: string) => {
+  //     console.log(receivedMessage);
+  //   });
+  // }
+
+  // // Disconnects socket connection
+  // disconnectSocket(): any {
+  //   return this.websocketService.disconnetSocket();
+  // }
 }
